28e8479aad9c241054f28abccee08a98473b6b25
lulodelicia <lucadelicia05@gmail.com>
2022-07-19 15:41:54 -0300

    Añadimos los tipos numericos

--- python/tipos_de_datos.py
+++ python/tipos_de_datos.py
@@ -1,1 +1,13 @@
 ### TIPOS DE DATOS EN PYTHON ###
+
+"""
+	
+TIPOS NUMERICOS:
+
+	1 - Enteros(int): Representan todos los números enteros 
+	(positivos, negativos y 0), sin coma.
+
+	2 - Con coma(float): Sirve para representar todos los numeros incluidos
+	los numeros con coma.
+
+"""
