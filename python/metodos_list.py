fc53a20252fed83d409db460a1830a0c22f005c4
juldelicia <julietadelicia@gmail.com>
2022-07-19 17:17:11 -0300

    añadi el metodo append

--- python/metodos_list.py
+++ python/metodos_list.py
@@ -1,1 +1,13 @@
 ### METODOS DE LISTAS ###
+
+"""
+# METODO append()
+## Añade un elemento al final de la lista
+
+lista = ["felipe", "carlitos", "eugenia"]
+
+lista.append("julieta")
+
+print(lista)
+"""
+
