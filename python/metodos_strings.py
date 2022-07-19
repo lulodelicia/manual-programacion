3362c05ce4751d4cd7ce8db23954591adde5f176
lulodelicia <lucadelicia05@gmail.com>
2022-07-19 16:49:11 -0300

    tipo dic

---
+++ python/metodos_strings.py
@@ -0,0 +1,37 @@
+### METODOS CON LOS STRINGS ###
+
+"""
+# METODO upper()
+## Pone todo el string en mayusculas
+
+luca = "hola"
+
+mayus = luca.upper()
+
+print(mayus)
+"""
+
+"""
+# METODO join()
+## El metodo join une todos los elementos de una lista y usa el string
+## como separador
+
+futbol = ["messi", "ronaldo","luca"]
+
+string = "pepito"
+
+x = string.join(futbol)
+
+print(x)
+"""
+
+"""
+# METODO lower()
+##el metodo lower convierte todo en minusculas
+x = "hola mundo"
+
+luca = x.lower()
+
+print(luca)
+"""
+
