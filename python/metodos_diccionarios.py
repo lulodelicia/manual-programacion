575cebfa059ab5631dad9578fd8bab8ab21812d6
felipendelicia <delicia4581@gmail.com>
2022-07-20 14:23:15 -0300

    copy and clear

---
+++ python/metodos_diccionarios.py
@@ -0,0 +1,30 @@
+### METODOS DE DICCIONARIO ###
+
+"""
+# METODO clear()
+## Elimina todos los elementos del diccionario
+
+dic = {
+    "color":"rojo",
+    "estatura":180
+}
+
+dic.clear()
+
+print(dic)
+"""
+
+"""
+# METODO copy()
+## Devuelve una copia del diccionario
+
+dic = {
+    "color":"rojo",
+    "estatura":180
+}
+
+dic_copy = dic.copy
+
+print(dic_copy)
+print(dic)
+"""
