cf24ca5c193d59f7b7624e6a20486df76e99cca7
felipendelicia <delicia4581@gmail.com>
2022-07-20 14:27:33 -0300

    get and fromkeys

--- python/metodos_diccionarios.py
+++ python/metodos_diccionarios.py
@@ -28,3 +28,30 @@ dic_copy = dic.copy
 print(dic_copy)
 print(dic)
 """
+
+"""
+# METODO fromkeys()
+## Crea un diccionario a partir de una lista y un solo valor
+
+x = ['key1', 'key2', 'key3']
+
+y = 0
+
+diccionario = dict.fromkeys(x, y)
+
+print(diccionario)
+"""
+
+"""
+# METODO get()
+## Te devuelve el valor de la clave que vos le digas
+
+dic = {
+    "color":"rojo",
+    "estatura":180
+}
+
+x = dic.get("color")
+
+print(x)
+"""
